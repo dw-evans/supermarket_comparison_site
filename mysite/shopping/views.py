df9905cc1815bb9b1fcaa22fe21aba1eb626c517
dw-evans <106231868+dw-evans@users.noreply.github.com>
2022-12-13 23:54:14 +0000

    Styling additions

--- mysite/shopping/views.py
+++ mysite/shopping/views.py
@@ -13,7 +13,7 @@ def home(request):
 
         items = item_list = [
             WaitroseItem(item)
-            for item in WaitroseRequest(query, max_items=10).get_items_as_list()
+            for item in WaitroseRequest(query, max_items=100).get_items_as_list()
         ]
 
         items = [item for item in items if not item.is_null]
