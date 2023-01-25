5d9ddb8a6927edc83d34960e791067556cb5cc28
dw-evans <106231868+dw-evans@users.noreply.github.com>
2023-01-25 22:02:04 +0000

    basic waitrose proto complete

--- mysite/shopping/views.py
+++ mysite/shopping/views.py
@@ -34,7 +34,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 class Session:
     def __init__(self):
         self.query: str = ""
-        self.max_items: int = 10
+        self.max_items: int = 100
         self.items: list[Item] = []
         self.cart_items: list[CartItem] = []
         self.search_result: SearchResult = SearchResult([])
@@ -75,13 +75,64 @@ def to_nzd(obj: Price):
 setattr(Price, "to_nzd", to_nzd)
 
 
+def add_item_to_cart_by_id(item_identifier: str):
+    filter_result = list(
+        filter(
+            lambda x: str(x.identifier) == item_identifier, s.search_result.initial_list
+        )
+    )
+    if len(filter_result) == 1:
+
+        item = filter_result[0]
+
+        # crudely ignore doubling adds to cart
+        if item not in [cart_item.item for cart_item in s.cart_items]:
+            print(f"adding id={item_identifier} to cart")
+            s.cart_items.append(CartItem(item, pcs=1))
+
+        else:
+            filter_result = list(
+                filter(
+                    lambda cart_item: str(cart_item.item.identifier) == item_identifier,
+                    s.cart_items,
+                )
+            )
+            if len(filter_result) > 0:
+                cart_item = filter_result[0]
+                cart_item.pcs += 1
+
+
+def remove_item_from_cart_by_id(item_identifier: str):
+    # make sure to get the uuid as a string
+    filter_result = list(
+        filter(lambda x: str(x.item.identifier) == item_identifier, s.cart_items)
+    )
+
+    if len(filter_result) > 0:
+
+        item = filter_result[0]
+
+        # crudely ignore doubling adds to cart
+        if item in s.cart_items:
+            if item.pcs > 1:
+                item.pcs -= 1
+            else:
+                s.cart_items.remove(item)
+
+
 s = Session()
 
 
 def home(request):
 
     if request.method == "POST":
-        # print(request.POST)
+        print(request.POST)
+
+        if "add_to_cart" in request.POST:
+            add_item_to_cart_by_id(request.POST.get("add_to_cart"))
+
+        if "remove_from_cart" in request.POST:
+            remove_item_from_cart_by_id(request.POST.get("remove_from_cart"))
 
         if "sort_by" in request.POST:
             sort_by_val = request.POST.get("sort_by")
@@ -121,7 +172,7 @@ def home(request):
     s.search_result.filter_and_sort(s.filter)
 
     context = {
-        "search_term": s.query,
+        "query": s.query,
         "item_list": s.search_result.sorted_and_filtered_list,
         "cart_item_list": s.cart_items,
     }
